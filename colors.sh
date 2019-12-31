636dbb2df8a186d1e9efdd980cde5871b02a6129
Amit Kumar Gautam <gautamamits95@gmail.com>
2019-12-31 17:31:03 +0530

    python instead of python3

--- colors.sh
+++ colors.sh
@@ -1,6 +1,6 @@
 curl -s https://gist.githubusercontent.com/HaleTom/89ffe32783f89f403bba96bd7bcd1263/raw/ | bash
 
-curl -s https://gist.githubusercontent.com/justinabrahms/1047767/raw/a79218b6ca8c1c04856968d2d202510a4f7ec215/colortest.py | python3
+curl -s https://gist.githubusercontent.com/justinabrahms/1047767/raw/a79218b6ca8c1c04856968d2d202510a4f7ec215/colortest.py | python
 
 curl -s https://gitlab.gnome.org/GNOME/vte/raw/vte-0-38/perf/256test.sh | bash
 
