8bd9bffa3d64ab82b0f1bb757dd8370d6ccbdb20
amit <gautamamits95@gmail.com>
2020-01-05 01:57:53 +0530

    fakelogger is working

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
-var cities = {cities:["Amsterdam","Berlin","New York","San Francisco","Tokyo"]}
+var cities = { cities: ["Amsterdam", "Berlin", "New York", "San Francisco", "Tokyo"] }
+const fakelog = require('./fakelog.json')
+var logIndex = 0
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(cities));
     res.end();
@@ -11,22 +13,26 @@ app.get("/error", (req, res) => {
     res.writeHead(500, { 'Content-Type': 'application/json' });
     res.end();
 })
-app.get('/health', (req, res) => res.send({"status": "OK", "time": new Date()}))
+app.get('/health', (req, res) => res.send({ "status": "OK", "time": new Date() }))
 var port = process.env.PORT || 8080;
 app.listen(port);
 
-setInterval(function(str1, str2) {
-  console.log(str1 + " " + str2 + " "+makeid(10));
-}, 800, "Hello.", "How are you?");
+setInterval(function() {
+    if (logIndex >= fakelog.length) {
+        logIndex = 0;
+    }
+    console.log(fakelog[logIndex].data.result.content)
+    logIndex += 1;
+}, 100);
 
 function makeid(length) {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < length; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    for (var i = 0; i < length; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-  return text;
+    return text;
 }
 
 module.exports = app;
