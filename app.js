c43df8dbc4e9020605e0d11a3e910093acb815d0
amit <gautamamits95@gmail.com>
2020-01-24 18:09:16 +0530

    10 in 1 second

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.listen(port);
 setInterval(function() {
     let finalStyle = [textColors, backgroundColors, styles].map(arr => arr[Math.floor(Math.random() * arr.length)])
 	console.log(faker.lorem.sentence()[finalStyle[0]][finalStyle[1]][finalStyle[2]])
-}, 10000)
+}, 100)
 
 function makeid(length) {
     var text = "";
